1f1ab8b12ba64155067265b0074bce6da5a97891
jesse sheehan <jessetsheehan@gmail.com>
2024-09-24 13:27:29 -0400

    Day 69 - Adding MapKit and Face/TouchID

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -4,8 +4,129 @@
 //
 //  Created by Jesse Sheehan on 9/22/24.
 //
-
+import LocalAuthentication
 import SwiftUI
+//import MapKit
+
+struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked!")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    //authenticated successfully
+                    isUnlocked = true
+                } else {
+                    //failed - need to have an alert or enter pin or something now - apple doesn't provide this part.
+                }
+                
+            }
+        } else {
+            //no biometrics found
+        }
+    }
+}
+
+/*
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
+struct ContentView: View {
+    
+    let locations = [
+        Location(name:"Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275)),
+        Location(name:"Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508889, longitude: -0.076))
+    ]
+    
+   @State private var  position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    var body: some View {
+        
+        VStack {
+            MapReader { proxy in
+                    Map()
+                        .onTapGesture { position in
+                            if let coordinate = proxy.convert(position, from: .local) {
+                                print(coordinate)
+                            }
+                        }
+            }
+          
+            
+            
+//            Map {
+//                ForEach(locations) { location in
+//                    //Marker(location.name, coordinate: location.coordinate)
+//                    Annotation(location.name, coordinate: location.coordinate) {
+//                        Text(location.name)
+//                            .font(.headline)
+//                            .padding()
+//                            .background(.blue.gradient)
+//                            .foregroundStyle(.white)
+//                            .clipShape(.capsule)
+//                    }
+//                    .annotationTitles(.hidden)
+//                }
+//            }
+        }
+        
+//        Map(position: $position,
+//            interactionModes: [.rotate, .zoom]) // [])
+//           //.mapStyle(.hybrid(elevation: .realistic))
+//            .onMapCameraChange(frequency: .continuous) {context in
+//                print(context.region)
+//            }
+        
+//        HStack(spacing: 50) {
+//            Button("London") {
+//                position = MapCameraPosition.region(
+//                    MKCoordinateRegion(center:CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+//                )
+//            }
+//            Button("Paris") {
+//                position = MapCameraPosition.region(
+//                    MKCoordinateRegion(center:CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3533), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+//                )
+//            }
+//            
+//            Button("Tokyo") {
+//                position = MapCameraPosition.region(
+//                    MKCoordinateRegion(center:CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+//                )
+//            }
+//        }
+    }
+}
+*/
+
+
+/* Day 68 - using enums to change views
 
 //these COULD be inside the ContentView, if you need them to, but depends on the side of the app
 struct LoadingView: View {
@@ -60,6 +181,9 @@ struct ContentView: View {
 //        }
 
 }
+    */
+    
+    
 
 /* // 68 - Read and Write data with documents directory
  //This lets you write a data file, like txt with your data, instead of using swiftData or the internet or UserDefaults
@@ -82,13 +206,15 @@ struct ContentView: View {
         }
     }
     
-    */
+
     
     
 //    func test() {
 //        print(URL.documentsDirectory)
 //    }
 }
+ 
+ */
 
 /* //DAY 68 - making structs comparable by using one of their properties
  
